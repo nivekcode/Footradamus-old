b08a47a3e38635c088942c365cae68eeee752723
kreuzerk <kevin.kreuzer90@icloud.com>
2016-11-10 07:29:37 +0100

    kk: Component for the TeamSelection added

---
+++ src/app/team-selection/teamSelection.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by kevinkreuzer on 10.11.16.
+ */
+
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'team-selection',
+  templateUrl: './teamSelection.html'
+})
+export default class TeamSelectionComponent{
+
+}
---
+++ src/app/team-selection/teamSelection.module.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by kevinkreuzer on 10.11.16.
+ */
+
+import {NgModule} from "@angular/core";
+import TeamSelectionComponent from "./teamSelection.component";
+
+@NgModule({
+  declarations: [TeamSelectionComponent],
+  exports: [TeamSelectionComponent]
+})
+export default class TeamSelectionModule{}

7f498646d17ef9d2f3348b3573c4f81cbe75b913
kreuzerk <kevin.kreuzer90@icloud.com>
2016-11-28 08:08:15 +0100

    kk: Create Dev Configs - use Dev Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,9 @@
-export const CONFIG = {
+export const PROD_CONFIG = {
   backendUrl: 'http://api.football-api.com/2.0/',
   authParam: '?Authorization=565ec012251f932ea4000001393b4115a8bf4bf551672b0543e35683'
+}
+
+export const DEV_CONFIG = {
+  backendUrl: 'http://localhost:3000/',
+  authParam: ''
 }

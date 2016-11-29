3096f04bb5952b48e1679f2cf9b44e3d9f325095
kreuzerk <kevin.kreuzer90@icloud.com>
2016-11-29 06:55:18 +0100

    kk: Remove console.logs

--- src/app/prediction/prediction.service.ts
+++ src/app/prediction/prediction.service.ts
@@ -27,31 +27,20 @@ export default class PredictionService {
 
   private _predictResult(homeTeamStats, awayTeamStats) {
     let homeTeamPoints = this._getPointsForTeam(homeTeamStats, true);
-    console.log('-------------------------');
     let awayTeamPoints = this._getPointsForTeam(awayTeamStats, false);
-    console.log('Home Team Points', homeTeamPoints);
-    console.log('Away Team Points', awayTeamPoints);
     return homeTeamPoints > awayTeamPoints;
   }
 
   private _getPointsForTeam(teamstats, isHomeTeam: boolean) {
     let points: number = 0;
     points += this._getWinLosDiff(teamstats);
-    console.log('Wins', points);
     points += isHomeTeam ? this._getHomeWinnLosDiff(teamstats) : this._getAwayWinnLosDiff(teamstats);
-    console.log('HomeWins', points);
     points += this._getCleanSheets(teamstats);
-    console.log('Clean', points);
     points += isHomeTeam ? this._getHomeCleanSheets(teamstats) : this._getAwayCleanSheets(teamstats);
-    console.log('Clean Home', points);
     points += this._getGoalDiff(teamstats);
-    console.log('GoalDiff', points);
     points += isHomeTeam ? this._getHomeGoalsDiff(teamstats) : this._getAwayGoalsDiff(teamstats);
-    console.log('HomeGoals', points);
     points += this._getFirstHalfGoals(teamstats);
-    console.log('First Half Goals', points);
     points += this._getFirst75MinutesGoals(teamstats);
-    console.log('75 Goals', points);
 
     return points;
   }
@@ -96,7 +85,6 @@ export default class PredictionService {
     let percentageFHGoals = this._extractNumberFromPercentage(teamstats.scoring_minutes_0_15_pct) +
       this._extractNumberFromPercentage(teamstats.scoring_minutes_15_30_pct) +
       this._extractNumberFromPercentage(teamstats.scoring_minutes_30_45_pct);
-    console.log('Halftime', percentageFHGoals);
     return 5 / 100 * percentageFHGoals;
   }
 
@@ -106,9 +94,6 @@ export default class PredictionService {
       this._extractNumberFromPercentage(teamstats.scoring_minutes_30_45_pct) +
       this._extractNumberFromPercentage(teamstats.scoring_minutes_45_60_pct) +
       this._extractNumberFromPercentage(teamstats.scoring_minutes_60_75_pct);
-
-    console.log('75', percentage75Goals);
-
     return 5 / 100 * percentage75Goals;
   }
 

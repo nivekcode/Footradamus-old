ec8229dde505d7a4c3f861a8b808f03473684adc
kreuzerk <kevin.kreuzer90@icloud.com>
2016-12-07 20:26:12 +0100

    kk: Prepare League Selector

--- src/app/prediction/match-selection/matchSelection.module.ts
+++ src/app/prediction/match-selection/matchSelection.module.ts
@@ -5,13 +5,14 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import MatchSelectionComponent from "./matchSelection.component";
 import TeamSelectionComponent from "./team-selection/teamSelection.component";
-import SelectorComponent from "./team-selection/selector/selector.component";
+import SelectorComponent from "./team-selection/team-selector/teamSelector.component";
 import ClubLogoComponent from "./team-selection/club-logo/clubLogo.component";
 import TeamSelectionService from "./team-selection/teamSelection.service";
+import LeagueSelectionComponent from "./team-selection/league-selection/leagueSelection.component";
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [MatchSelectionComponent, TeamSelectionComponent, SelectorComponent, ClubLogoComponent],
+  declarations: [MatchSelectionComponent, TeamSelectionComponent, SelectorComponent, ClubLogoComponent, LeagueSelectionComponent],
   exports: [MatchSelectionComponent],
   providers: [TeamSelectionService]
 })
---
+++ src/app/prediction/match-selection/team-selection/league-selection/leagueSelection.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by kevinkreuzer on 07.12.16.
+ */
+
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'league-selection',
+  templateUrl: 'leagueSelection.html',
+  styleUrls: ['leagueSelection.css']
+})
+export default class LeagueSelectionComponent {
+
+}

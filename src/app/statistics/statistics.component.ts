e6cbfb3b89757147015d212ae920029cacb72fca
kreuzerk <kevin.kreuzer90@icloud.com>
2016-12-22 14:15:18 +0100

    kk: Chart added

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -10,4 +10,17 @@ import {Component} from "@angular/core";
 })
 export default class StatisticsComponent {
 
+  // Doughnut
+  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
+
+  // events
+  public chartClicked(e:any):void {
+    console.log(e);
+  }
+
+  public chartHovered(e:any):void {
+    console.log(e);
+  }
 }

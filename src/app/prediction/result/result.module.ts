aa2e2e5fd8fc8b6d2e9c434e7c74f884b7646d38
kreuzerk <kevin.kreuzer90@icloud.com>
2016-12-24 14:26:22 +0100

    kk: Added a submitPrediction Component

--- src/app/prediction/result/result.module.ts
+++ src/app/prediction/result/result.module.ts
@@ -6,9 +6,10 @@ import {NgModule} from "@angular/core";
 import ResultComponent from "./result.component";
 import WinnerLogoComponent from "./winner-logo/winnerLogo.component";
 import {CommonModule} from "@angular/common";
+import SubmitComponent from "./submit/submitPrediction.component";
 
 @NgModule({
-  declarations: [ResultComponent, WinnerLogoComponent],
+  declarations: [ResultComponent, WinnerLogoComponent, SubmitComponent],
   exports: [ResultComponent],
   imports: [CommonModule]
 })
---
+++ src/app/prediction/result/submit/submitPrediction.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by kevinkreuzer on 24.12.16.
+ */
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'submit-prediction',
+  templateUrl: 'submitPrediction.html'
+})
+export default class SubmitComponent{
+
+}

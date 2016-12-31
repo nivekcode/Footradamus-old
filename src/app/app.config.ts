a588f8c66f35ce4f4f163bb0501f4a40c51d12c9
kreuzerk <kevin.kreuzer90@icloud.com>
2016-12-31 17:06:13 +0100

    kk: Save prediction to json File

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 export const PROD_CONFIG = {
   backendUrl: 'http://api.football-api.com/2.0/',
-  authParam: '?Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76'
+  authParam: '?Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76',
+  predictionBackendUrl: 'http://localhost:3004'
 }
 
 export const DEV_CONFIG = {
   backendUrl: 'http://localhost:3000/',
-  authParam: ''
+  authParam: '',
+  predictionBackendUrl: 'http://localhost:3004/predictions'
 }

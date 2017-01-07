c81c3157242d90a622e6b0ddf788c4004cf5df1f
kreuzerk <kevin.kreuzer90@icloud.com>
2017-01-07 19:55:32 +0100

    kk: SelectionService added and green

--- src/app/prediction/match-selection/matchSelection.service.spec.ts
+++ src/app/prediction/match-selection/matchSelection.service.spec.ts
@@ -3,11 +3,13 @@
  */
 
 import {TestBed, inject} from "@angular/core/testing";
-import {HttpModule, Http, BaseRequestOptions, ResponseOptions, RequestOptions, Response} from "@angular/http";
+import {HttpModule, Http, BaseRequestOptions, ResponseOptions, Response} from "@angular/http";
 import MatchSelectionService from "./matchSelection.service";
 import {MockBackend} from "@angular/http/testing";
 import PremierLeagueLogos from "../logos/clubs/premierLeagueLogos.service";
 import LogoService from "../logos/clubs/logoDispatcher.service";
+import LaLigaLogos from "../logos/clubs/laLigaLogos.service";
+import BundesligaLogos from "../logos/clubs/bundesligaLogos.service";
 
 describe('Match Selection Service', () => {
 
@@ -17,6 +19,8 @@ describe('Match Selection Service', () => {
       providers: [
         {provide: 'config', useValue: 'http://unitTest:3000/'},
         LogoService,
+        LaLigaLogos,
+        BundesligaLogos,
         PremierLeagueLogos,
         MatchSelectionService,
         {
@@ -34,25 +38,25 @@ describe('Match Selection Service', () => {
   it('should get the leagues and filter them to Bundesliga, Premier League and Primera Division',
     inject([MatchSelectionService, MockBackend], (sut, mockBackend) => {
 
-      const mockResponse = {
-        data: [
-          {
-            "id": "1399",
-            "name": "Primera División",
-            "region": "Spain"
-          },
-          {
-            "id": "1408",
-            "name": "Super League",
-            "region": "Switzerland"
-          },
-          {
-            "id": "1425",
-            "name": "Süper Lig",
-            "region": "Turkey"
-          }
-        ]
-      };
+      let expectedLeague = {id: '1399', name: 'Primera División', region: 'Spain'};
+
+      const mockResponse = [
+        {
+          "id": "1399",
+          "name": "Primera División",
+          "region": "Spain"
+        },
+        {
+          "id": "1408",
+          "name": "Super League",
+          "region": "Switzerland"
+        },
+        {
+          "id": "1425",
+          "name": "Süper Lig",
+          "region": "Turkey"
+        }
+      ];
 
       mockBackend.connections.subscribe((connection) => {
         connection.mockRespond(new Response(new ResponseOptions({
@@ -62,9 +66,8 @@ describe('Match Selection Service', () => {
 
       sut.getLeagues()
         .subscribe(res => {
-          console.log('Hier');
-          console.log('This is the response', res);
-          expect(true).toBeTrue();
-        })
+          let actualLeague = res[0];
+          expect(expectedLeague).toEqual(actualLeague);
+        });
     }));
 });

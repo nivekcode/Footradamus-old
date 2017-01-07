bd7b8fbf4086ed21cf4afd5093f14c47a96e0e0e
kreuzerk <kevin.kreuzer90@icloud.com>
2017-01-07 14:50:38 +0100

    kk: Configured testing module

--- src/app/prediction/match-selection/matchSelection.module.ts
+++ src/app/prediction/match-selection/matchSelection.module.ts
@@ -7,14 +7,14 @@ import MatchSelectionComponent from "./matchSelection.component";
 import TeamSelectionComponent from "./team-selection/teamSelection.component";
 import SelectorComponent from "./team-selection/team-selector/teamSelector.component";
 import ClubLogoComponent from "./team-selection/club-logo/clubLogo.component";
-import TeamSelectionService from "./matchSelection.service";
+import MatchSelectionService from "./matchSelection.service";
 import LeagueSelectionComponent from "./league-selection/leagueSelection.component";
 
 @NgModule({
   imports: [CommonModule],
   declarations: [MatchSelectionComponent, TeamSelectionComponent, SelectorComponent, ClubLogoComponent, LeagueSelectionComponent],
   exports: [MatchSelectionComponent],
-  providers: [TeamSelectionService]
+  providers: [MatchSelectionService]
 })
 export default class MatchSelectionModule{
 }
--- src/app/prediction/match-selection/matchSelection.service.spec.ts
+++ src/app/prediction/match-selection/matchSelection.service.spec.ts
@@ -1,13 +1,31 @@
+import {TestBed} from "@angular/core/testing";
+import {HttpModule, Http, BaseRequestOptions} from "@angular/http";
+import MatchSelectionService from "./matchSelection.service";
+import LogoService from "../logos/clubs/logoDispatcher.service";
+import {MockBackend} from "@angular/http/testing";
 /**
  * Created by kevinkreuzer on 07.01.17.
  */
 
 describe('Match Selection Service', () => {
 
-  describe('HTTP Call', () => {
-
-    it('must be green', () => {
-      expect(true).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [
+        { provide: 'config', useValue: 'http://testurl.com'},
+        LogoService,
+        MatchSelectionService,
+        {
+          provide: Http,
+          useFactory: (mockBackend, options) => new Http(mockBackend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ]
     });
+  });
+
+  it('http call', () => {
+    expect(true).toBeTruthy();
   });
 });

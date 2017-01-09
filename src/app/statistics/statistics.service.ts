acbbc85f2ca4562814d408fa84feac0f570dbc78
kreuzerk <kevin.kreuzer90@icloud.com>
2017-01-09 08:10:51 +0100

    kk: Calculate if prediction was correct

--- src/app/statistics/statistics.service.ts
+++ src/app/statistics/statistics.service.ts
@@ -5,11 +5,18 @@
 import {Injectable, Inject} from "@angular/core";
 import {Http} from "@angular/http";
 import prediction from "../model/prediction.model";
+import predictionStatistics from "../model/predictionStatistics.model";
 
 @Injectable()
 export default class StatisticsService {
 
+  private predictionStatistics: predictionStatistics;
+
   constructor(private http: Http, @Inject('config') private config) {
+    this.predictionStatistics = {
+      predictedCorrectly: 0,
+      predictedFalsy: 0
+    };
   }
 
   getStatistics() {
@@ -18,10 +25,37 @@ export default class StatisticsService {
         predictions.forEach((prediction: prediction) => {
           this._getMatchStatistics(prediction)
             .subscribe(matchStatistics => {
-              console.log('MatchStatistics', matchStatistics);
+              this._calculateStats(prediction, matchStatistics[0]);
             })
         })
       });
+  }
+
+  private _calculateStats(prediction: prediction, matchStatistics){
+    let actualWinner = this._getWinningTeam(matchStatistics);
+    let wasPredictionCorrect = this._wasPredictionCorrect(prediction.winner, actualWinner);
+    if(wasPredictionCorrect){
+      this.predictionStatistics.predictedCorrectly++;
+    }
+    else{
+      this.predictionStatistics.predictedFalsy;
+    }
+  }
+
+  private _getWinningTeam(matchStatistics){
+    let homeTeamScore: number = parseInt(matchStatistics.localteam_score);
+    let awayTeamScore: number = parseInt(matchStatistics.visitorteam_score);
+
+    if(awayTeamScore > homeTeamScore){
+      return matchStatistics.localteam_name;
+    }
+    else{
+      return matchStatistics.visitorteam_name;
+    }
+  }
+
+  private _wasPredictionCorrect(predictedWinner, actualWinner){
+    return predictedWinner.toLowerCase() === actualWinner.toLowerCase();
   }
 
   private _getStoredPredictions() {

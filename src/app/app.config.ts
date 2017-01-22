afc48a9d2ae20cbcb92c578785cb3f2ffc8bee3b
kreuzerk <kevin.kreuzer90@icloud.com>
2017-01-22 16:24:09 +0100

    kk: Add Routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ const predictionBackendUrl = 'http://localhost:3004/predictions';
 
 export const PROD_CONFIG = {
   backendUrl: 'http://api.football-api.com/2.0/',
-  authParam: '?Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76',
+  authParam: 'Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76',
   predictionBackendUrl: predictionBackendUrl
 }
 
---
+++ src/app/predictionList/predictionList.routes.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by kevinkreuzer on 22.01.17.
+ */
+import {Route, RouterModule} from "@angular/router";
+import PredictionListComponent from "./predictionList.component";
+
+const predictionListRoutes: Array<Route> = [
+  {path: '', component: PredictionListComponent}
+];
+
+export default RouterModule.forChild(predictionListRoutes);

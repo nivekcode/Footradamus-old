0e6237d5fd9199ddbf3184d1d6dedf099963e2f6
kreuzerk <kevin.kreuzer90@icloud.com>
2017-02-03 18:01:44 +0100

    Added put method to the servere

--- footradamus-server/server.js
+++ footradamus-server/server.js
@@ -1,9 +1,33 @@
 let express = require('express');
-let predictions = require('./predictions.json');
+let bodyParser = require('body-parser');
+let fs = require('fs');
+let predictionsJSON = require('./predictions.json');
+
 let app = express();
 
-app.get('/predictions', (request, resposne) => {
-  resposne.send(predictions);
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/predictions', (request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+    response.send(predictionsJSON.predictions);
+});
+
+app.put('/predictions/:id', (request, response) => {
+    let updatedPrediction = request.body;
+    let id = request.params.id;
+
+    predictionsJSON.predictions.forEach((predcition, index) => {
+      if(parseInt(id) === predcition.id){
+        let predictionToUpdate = predictionsJSON.predictions[index];
+        predictionsJSON.predictions[index] = Object.assign({}, predictionToUpdate, updatedPrediction);
+      }
+    });
+
+    fs.writeFileSync('./predictions.json', JSON.stringify({predictions: predictionsJSON.predictions}));
+    response.send('Update Successfull');
 });
 
 app.listen(3004, () => console.log('Server is up and running'));

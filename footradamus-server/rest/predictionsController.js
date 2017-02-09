976c037cd9a2b54c97fc9f93343efd1eb1fc696d
kreuzerk <kevin.kreuzer90@icloud.com>
2017-02-09 09:16:46 +0100

    added CRUD operations with mongoose

--- footradamus-server/rest/predictionsController.js
+++ footradamus-server/rest/predictionsController.js
@@ -24,60 +24,34 @@ app.get('/predictions', (request, response) => {
 });
 
 app.put('/predictions/:id', (request, response) => {
-    let updatedPrediction = request.body;
     let id = request.params.id;
-    let predictionToUpdate;
-
-    predictions.forEach((prediction, index) => {
-        if (parseInt(id) === prediction.id) {
-            predictionToUpdate = predictions[index];
-            updatedPrediction.id = prediction.id;
-            predictions[index] = Object.assign({}, predictionToUpdate, updatedPrediction);
-        }
+    predictionsModel.findOneAndUpdate({_id: id}, request.body, (error, prediciton) => {
+      if (!error) {
+          response.setHeader('Content-Type', 'application/json');
+          response.send(request.body);
+      } else {
+          response.status(404).send(`Unfortunatley no prediction with id ${id} was found`);
+      }
     });
-    writePrediction(predictions);
-
-    if (predictionToUpdate) {
-        response.setHeader('Content-Type', 'application/json');
-        response.send(updatedPrediction);
-    } else {
-        response.status(404).send(`Unfortunatley no prediction with id ${id} was found`);
-    }
 });
 
 app.post('/predictions', (request, response) => {
     let newPrediction = request.body;
-    newPrediction.id = parseInt(predictions[predictions.length - 1].id) + 1;
-    predictions.push(newPrediction);
-
-    writePrediction(predictions);
+    predictionsModel.create(newPrediction);
     response.setHeader('Content-Type', 'application/json');
     response.send(newPrediction);
 });
 
 app.delete('/predictions/:id', (request, response) => {
     let id = request.params.id;
-    let deletedPrediction;
-
-    predictions.forEach((prediction, index) => {
-        if (parseInt(id) === prediction.id) {
-            deletedPrediction = prediction;
-            predictions.splice(index, 1);
-        }
+    predictionsModel.findOneAndRemove({_id: id}, (error, deletedPrediction) => {
+      if (!error) {
+          response.setHeader('Content-Type', 'application/json');
+          response.send(deletedPrediction);
+      } else {
+          response.status(404).send(`Unfortunatley no prediction with id ${id} was found`);
+      }
     });
-
-    writePrediction(predictions);
-
-    if (deletedPrediction) {
-        response.setHeader('Content-Type', 'application/json');
-        response.send(deletedPrediction);
-    } else {
-        response.status(404).send(`Unfortunatley no prediction with id ${id} was found`);
-    }
 });
-
-writePrediction = function(predictions) {
-    fs.writeFileSync('./rest/predictions.json', JSON.stringify({predictions: predictions}));
-}
 
 app.listen(3004, () => console.log('Server is up and running'));

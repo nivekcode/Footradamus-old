573544a1e404f283e64454e94ba81f04430799ea
kreuzerk <kevin.kreuzer90@icloud.com>
2017-02-14 17:27:36 +0100

    Changed the prod url

--- footradamus-client/src/environments/environment.prod.ts
+++ footradamus-client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   backendUrl: 'http://api.football-api.com/2.0/',
   authParam: 'Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76',
-  predictionBackendUrl: 'http://localhost:3004/predictions'
+  predictionBackendUrl: 'https://intense-bastion-56637.herokuapp.com/predictions'
 };

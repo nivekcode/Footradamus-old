6319aeb1f440268b6f889be528a6bc82d4f1a9a7
kreuzerk <kevin.kreuzer90@icloud.com>
2017-02-28 08:06:54 +0100

    kk: Adjusted front end urls

--- footradamus-client/src/environments/environment.prod.ts
+++ footradamus-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://intense-bastion-56637.herokuapp.com/',
+  backendUrl: 'https://intense-bastion-56637.herokuapp.com/footradamus/',
   authParam: 'Authorization=565ec012251f932ea4000001fa542ae9d994470e73fdb314a8a56d76',
   predictionBackendUrl: 'https://intense-bastion-56637.herokuapp.com/predictions'
 };

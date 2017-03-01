4e06790ebf3df004a1f7c93596fbfbdd558fffc6
kreuzerk <kevin.kreuzer90@icloud.com>
2017-03-01 20:48:49 +0100

    kk: Added dev and prod script

--- footradamus-server/src/app/db/db.js
+++ footradamus-server/src/app/db/db.js
@@ -1,11 +1,10 @@
-//let uri = 'mongodb://footradmin:footradmin@ds046549.mlab.com:46549/footradamus';
 let mongoose = require('mongoose');
 
 let createDB = (uri) => {
     mongoose.connect(uri);
     let db = mongoose.connection;
 
-    db.on('error', () => console.log('An error occured'));
+    db.on('error', () => console.log('An error occured by connectiong to the database'));
     db.once('open', () => console.log('Successfully connected to the database'));
 }
 

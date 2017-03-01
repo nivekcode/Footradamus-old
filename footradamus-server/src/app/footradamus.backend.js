5e8ae668506c60806628b5ddf7ffe5a43059b48b
kreuzerk <kevin.kreuzer90@icloud.com>
2017-03-01 20:26:45 +0100

    kk: Added CORS Filter to a separate file

--- footradamus-server/src/app/footradamus.backend.js
+++ footradamus-server/src/app/footradamus.backend.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let cors = require('express-cors');
+let corsFilter = require('./cors/cors.filter');
 let createPredictionsController = require('./rest/predictionsController');
 let createReverseProxy = require('./rest/footballAPI.reverseProxy.js');
 let configLoader = require('../environments/environmentConfig.loader');
@@ -10,16 +10,9 @@ let footradamus = express();
 footradamus.use(bodyParser.urlencoded({extended: true}));
 footradamus.use(bodyParser.json());
 
-footradamus.use(cors({
-    allowedOrigins: [
-        'localhost:4200',
-        'https://footradamus.firebaseapp.com'
-    ]
-}));
+corsFilter.createCorsFilter(footradamus);
 
 createPredictionsController(footradamus);
 createReverseProxy(footradamus);
-
-console.log('Environment', configLoader.loadEnvironment());
 
 footradamus.listen(process.env.PORT || 3004, () => console.log('Server is up and running'));

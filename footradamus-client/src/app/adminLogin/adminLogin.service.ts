41fdff99a123ab29ce499c20a88db68b8e39d621
kreuzerk <kevin.kreuzer90@icloud.com>
2017-03-09 16:49:38 +0100

    kk: Redirect after login

--- footradamus-client/src/app/adminLogin/adminLogin.service.ts
+++ footradamus-client/src/app/adminLogin/adminLogin.service.ts
@@ -5,13 +5,15 @@ import {Injectable, Inject} from "@angular/core";
 import {Http} from "@angular/http";
 import MessageService from "../shared/message/message.service";
 import LocalStorageService from "../shared/localStorage/localStorage.service";
+import {Router} from "@angular/router";
 
 @Injectable()
 export default class AdminLoginService {
 
   constructor(private http: Http, @Inject('config') private config,
               private messageService: MessageService,
-              private localStorageService: LocalStorageService) {
+              private localStorageService: LocalStorageService,
+              private router: Router) {
   }
 
   login(username: string, password: string): void {
@@ -21,7 +23,8 @@ export default class AdminLoginService {
       .subscribe(res => {
           this.localStorageService.setAdminLoginToken(res.json().token);
           this.messageService.showInfoMessage('Login successfull', 'Enjoy the amin functionality');
-        },
+          this.router.navigate(['/predictionList']);
+      },
         (error) => {
           this.messageService.showErrorMessage('Login failed', error.json().error);
         }

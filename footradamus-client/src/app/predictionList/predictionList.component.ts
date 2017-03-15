c0db5f1b62669724196c04a422a20998490bebca
kreuzerk <kevin.kreuzer90@icloud.com>
2017-03-15 08:15:15 +0100

    kk: Added toggling between mobile and desktop

--- footradamus-client/src/app/predictionList/predictionList.component.ts
+++ footradamus-client/src/app/predictionList/predictionList.component.ts
@@ -2,10 +2,17 @@
  * Created by kevinkreuzer on 15.03.17.
  */
 import {Component} from "@angular/core";
+import DeviceDetector from "../shared/deviceDetector/deviceDetector.service";
 
 @Component({
   selector: 'prediction-list',
   templateUrl: './predictionList.html'
 })
 export class PredictionListComponent{
+
+  protected $isMobileDevice;
+
+  constructor(private deviceDetector: DeviceDetector){
+    this.$isMobileDevice = this.deviceDetector.isMobileDevice();
+  }
 }

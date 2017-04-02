d454e3f60ab0c61a4310f993958f377f992b7b1c
kreuzerk <kevin.kreuzer90@icloud.com>
2017-04-02 18:24:46 +0200

    kk: Use TestBed for the setup

--- footradamus-client/src/app/prediction/shared/service/prediction.service.spec.ts
+++ footradamus-client/src/app/prediction/shared/service/prediction.service.spec.ts
@@ -1,9 +1,39 @@
 /**
  * Created by kevinkreuzer on 02.04.17.
  */
+import PredictionService from './prediction.service';
+import {TestBed, inject} from '@angular/core/testing';
+import StatisticsService from '../../../statistics/statistics.service';
+import {Store} from '@ngrx/store';
+
 describe('Prediction', () => {
 
+    let createPredictionService, getHomeTeamStats, getAwayTeamStats;
+
+    beforeEach(() => {
+
+        getHomeTeamStats = () => ({
+            test: 'test'
+        });
+
+        getAwayTeamStats = () => ({
+            test: 'test'
+        });
+
+        let statisticServiceMock = {}
+        let storeMock = {};
+        TestBed.configureTestingModule({
+            providers: [PredictionService,
+                {provide: StatisticsService, useValue: statisticServiceMock},
+                {provide: Store, useValue: storeMock}
+            ]
+        });
+    })
+
     it('must be true', () => {
-        expect(true).toBeTruthy();
+        inject([PredictionService], (sut) => {
+            //when
+            expect(true).toBeTruthy();
+        });
     });
 });
